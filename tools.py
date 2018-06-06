a2c577512e04c049120b166f4900ced1e2f80321
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2018-06-06 10:37:19 -0700

    added python files

---
+++ tools.py
@@ -0,0 +1,17 @@
+from math import pi, exp, pow
+import numpy as np 
+
+
+def calc_es(temperature):
+    es = 6.11*pow(10, (7.5*temperature)/(237.3+temperature))
+    return es
+
+
+def calc_ws(pressure, es):
+    ws = 621.97*(es/(pressure-es))
+    return ws
+
+
+def calc_rh(w, ws):
+    rh = w/ws
+    return rh
